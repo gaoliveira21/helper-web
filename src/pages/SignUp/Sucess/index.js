95e882a9691d7fda1acc866c8ab7f5b2d313f3cd
Pedro <pedro.lucas210600@gmail.com>
2020-10-04 17:41:02 -0300

    Pagina de sucesso do cadastro

--- src/pages/SignUp/Sucess/index.js
+++ src/pages/SignUp/Sucess/index.js
@@ -1,45 +1,24 @@
 import React from 'react';
 
-import { Container, ImageContent, Title, Image, FormContent } from './styles';
+import { Container, ImageContent, Title, Image, Content } from './styles';
 
-import registerImage from '../../../assets/images/register.svg';
+import checkCircle from '../../../assets/images/checkCircle.svg';
+import finishRegister from '../../../assets/images/finishRegister.svg';
 
 function SignIn() {
   return (
     <Container>
-      <FormContent>
-        <h1>Helper</h1>
-        <h2>Cadastrar-se</h2>
-        <p>Preencha os campos abaixo para efetuar o cadastro</p>
-        <form>
-          <label htmlFor="name">Nome da entidade</label>
-          <input
-            type="text"
-            placeholder="Digite o nome da entidade"
-            id="name"
-          />
-
-          <label htmlFor="email">E-mail</label>
-          <input type="text" placeholder="Digite seu E-mail" id="email" />
-
-          <label htmlFor="password">Senha</label>
-          <input type="password" placeholder="Digite sua senha" id="password" />
-
-          <button type="submit">Próximo</button>
-        </form>
-
-        <div>
-          <span />
-          <span />
-          <span />
-        </div>
-      </FormContent>
+      <Content>
+        <img src={checkCircle} alt="Cadastro concluído" />
+        <h1>Cadastro concluído com sucesso!</h1>
+        <a href="#!">Acessar o sistema</a>
+      </Content>
       <ImageContent>
         <Title>
           <h1>Helper</h1>
         </Title>
         <Image>
-          <img src={registerImage} alt="Cadastre-se" />
+          <img src={finishRegister} alt="Cadastre-se" />
         </Image>
       </ImageContent>
     </Container>
--- src/pages/SignUp/Sucess/styles.js
+++ src/pages/SignUp/Sucess/styles.js
@@ -13,7 +13,6 @@ export const ImageContent = styled.div`
     display: flex;
     flex-direction: column;
     width: 100%;
-    background: var(--color-primary);
   }
 `;
 
@@ -26,7 +25,7 @@ export const Title = styled.div`
 
   h1 {
     font-size: 4rem;
-    color: var(--color-white);
+    color: var(--color-primary);
   }
 `;
 
@@ -40,96 +39,32 @@ export const Image = styled.div`
   }
 `;
 
-export const FormContent = styled.div`
+export const Content = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   justify-content: center;
   width: 100%;
   padding: 0 40px;
 
   h1 {
-    margin-bottom: 4rem;
-    font-size: 4rem;
-    color: var(--color-primary);
+    margin: 2rem 0;
+    text-align: center;
   }
 
-  h2 {
-    font-size: 3.5rem;
-  }
+  a {
+    background: var(--color-primary);
+    padding: 16px 40px;
+    border: none;
+    border-radius: 4px;
+    color: var(--color-white);
+    font-weight: bold;
+    outline: none;
+    text-decoration: none;
+    text-align: center;
 
-  p {
-    margin-bottom: 4rem;
-    color: var(--color-gray);
-  }
-
-  form {
-    width: 100%;
-
-    label {
-      display: block;
-      margin: 8px 0px;
-      font-weight: bold;
-    }
-    input {
-      display: block;
-      width: 100%;
-      padding: 16px 24px;
-      border-radius: 4px;
-      border: none;
-      margin-bottom: 24px;
-      outline: none;
-    }
-
-    button {
-      width: 100%;
-      background: var(--color-primary);
-      padding: 16px 40px;
-      border: none;
-      border-radius: 4px;
-      color: var(--color-white);
-      font-weight: bold;
-      outline: none;
-
-      &:hover {
-        background: var(--color-primary-dark);
-      }
-    }
-  }
-
-  div {
-    display: flex;
-    flex-direction: row;
-    margin: 6rem auto;
-
-    span {
-      width: 24px;
-      height: 24px;
-      border-radius: 50%;
-      background-color: var(--color-gray);
-      margin: 0 2rem;
-    }
-
-    span:first-child {
-      background-color: var(--color-primary);
-    }
-  }
-
-  @media (min-width: 768px) {
-    h1 {
-      display: none;
-    }
-
-    h2 {
-      text-align: center;
-    }
-
-    p {
-      text-align: center;
-    }
-
-    form {
-      width: 500px;
-      margin: 0 auto;
+    &:hover {
+      background: var(--color-primary-dark);
     }
   }
 `;
