74605047d8bb255e4259a9dd2ed3faf661f64b69
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-10-10 14:40:29 -0300

    add /cases to routes

---
+++ src/pages/Cases/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import DefaultLayout from '../../layouts/Default';
+
+import { Container } from './styles';
+
+function Cases() {
+  return <DefaultLayout />;
+}
+
+export default Cases;
