a4e9628db750ce7ae13604c31cdfe341f7d61236
Pedro <pedro.lucas210600@gmail.com>
2020-10-17 15:57:49 -0300

    Criação do caso integrado com API

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -6,7 +6,7 @@ function Input({ width, label, name, type, placeholder, ...rest }) {
   return (
     <InputBlock width={width}>
       <label htmlFor={name}>{label}</label>
-      <input type={type} id={name} placeholder={placeholder} {...rest} />
+      <input type={type} id={name} name={name} placeholder={placeholder} {...rest} />
     </InputBlock>
   );
 }
