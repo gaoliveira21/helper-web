ddcd65ae431332d798b14b3c7fe015ae54ce7fb1
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2020-10-18 11:53:53 -0300

    add error to Textarea component

--- src/pages/Cases/NewCase/index.js
+++ src/pages/Cases/NewCase/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react'
+import { useHistory } from 'react-router-dom'
 
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import { toast } from 'react-toastify';
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import { toast } from 'react-toastify'
 
-import api from '../../../services/api';
+import api from '../../../services/api'
 
-import Input from '../../../components/Input';
-import Textarea from '../../../components/Textarea';
-import Header from '../../../components/Header';
+import Input from '../../../components/Input'
+import Textarea from '../../../components/Textarea'
+import Header from '../../../components/Header'
 
 import {
   Form,
   Container,
   InputContent,
   Dropzone,
-  ButtonContent,
-} from './styles';
+  ButtonContent
+} from './styles'
 
 const schema = Yup.object().shape({
   title: Yup.string()
@@ -27,23 +27,23 @@ const schema = Yup.object().shape({
     .positive()
     .min(5, 'Valor minímo preciso ser de 5 reais')
     .required('Insira o valor do caso'),
-  description: Yup.string().required('Insira uma breve descrição'),
-});
+  description: Yup.string().required('Insira uma breve descrição')
+})
 
-function NewCase() {
-  const history = useHistory();
+function NewCase () {
+  const history = useHistory()
 
-  function handleSubmit({ title, value, description }) {
+  function handleSubmit ({ title, value, description }) {
     try {
       api.post('/cases', {
         title,
         value,
-        description,
-      });
+        description
+      })
 
-      history.push('/dashboard');
+      history.push('/dashboard')
     } catch (error) {
-      toast.error('Erro ao cadastrar um novo caso');
+      toast.error('Erro ao cadastrar um novo caso')
     }
   }
 
@@ -51,66 +51,69 @@ function NewCase() {
     initialValues: {
       title: '',
       value: '',
-      description: '',
+      description: ''
     },
     validationSchema: schema,
     onSubmit: (values) => {
-      handleSubmit(values);
-    },
-  });
+      handleSubmit(values)
+    }
+  })
 
   return (
     <>
       <Header
-        title="Cadastrar novo caso"
-        description="Crie um novo caso para receber doações!"
-        label="Casos"
+        title='Cadastrar novo caso'
+        description='Crie um novo caso para receber doações!'
+        label='Casos'
       />
 
       <Container>
         <Form onSubmit={formik.handleSubmit}>
           <InputContent>
             <Input
-              width="70%"
-              label="Título do caso"
-              name="title"
-              type="text"
-              placeholder="Digite o título do caso"
+              width='70%'
+              label='Título do caso'
+              name='title'
+              type='text'
+              placeholder='Digite o título do caso'
               value={formik.values.title}
               onChange={formik.handleChange}
+              formik={formik}
             />
             <Input
-              width="30%"
-              label="Valor do caso R$"
-              name="value"
-              type="text"
-              placeholder="Digite o valor do caso"
+              width='30%'
+              label='Valor do caso R$'
+              name='value'
+              type='text'
+              placeholder='Digite o valor do caso'
               value={formik.values.value}
               onChange={formik.handleChange}
+              formik={formik}
             />
           </InputContent>
           <InputContent>
             <Textarea
-              label="Descrição do caso"
-              name="description"
-              placeholder="Faça uma breve descrição do caso"
+              label='Descrição do caso'
+              name='description'
+              placeholder='Faça uma breve descrição do caso'
               value={formik.values.description}
               onChange={formik.handleChange}
+              formik={formik}
             />
           </InputContent>
           <InputContent>
             <Dropzone>
-              <label htmlFor="images">Adicionar imagens ao caso</label>
+              <label htmlFor='images'>Adicionar imagens ao caso</label>
               <div>Dropzone</div>
             </Dropzone>
           </InputContent>
           <ButtonContent>
-            <button type="submit">Cadastrar</button>
+            <button type='submit'>Cadastrar</button>
           </ButtonContent>
         </Form>
       </Container>
     </>
-  );
+  )
 }
 
-export default NewCase;
+export default NewCase
